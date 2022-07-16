ee6f484a1fb869c1ac30887e4c02af9412017d8e
taehwajang <61370482+taehwajang@users.noreply.github.com>
2022-07-16 19:54:18 +0900

    feat: 로그인 버튼 구현

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,27 @@
-import { authService } from '../fbase';
-import AppRouter from './Router';
-import React, { useState } from 'react';
-function App() {
-    const [isLoggIn, setIsLoggIn] = useState(authService.currentUser);
+import { authService } from "../fbase";
 
-    return (
-        <>
-            <AppRouter isLoggIn={isLoggIn} />
-            <footer>&copy; {new Date().getFullYear()}Nwitter </footer>
-        </>
-    );
+import AppRouter from "./Router";
+import React, { useState, useEffect } from "react";
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggIn, setIsLoggIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggIn(true);
+      } else {
+        setIsLoggIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggIn={isLoggIn} /> : "Initializing...."}
+      <footer>&copy; {new Date().getFullYear()}Nwitter </footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,33 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const onChange = (e) => {
-        const {
-            target: { name, value },
-        } = e;
-        if (name === 'email') {
-            setEmail(value);
-        } else if (name === 'password') {
-            setPassword(value);
-        }
-    };
-    const onSubmit = (e) => {
-        e.preventDefault();
-    };
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} name="email" type="text" placeholder="Email" required value={email} />
-                <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
-                <input type="submit" value="log in" />
-            </form>
-            <div>
-                <button>continue with Google</button>
-                <button>continue with Github</button>
-            </div>
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    let data;
+    try {
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        //create new account
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+        />
+        <input
+          onChange={onChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "create Account" : "Sign In"}
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+      <div>
+        <button>continue with Google</button>
+        <button>continue with Github</button>
+      </div>
+    </div>
+  );
 };
 export default Auth;
